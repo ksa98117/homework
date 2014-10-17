7d116ba6f704bfa6325f3cb42d3e665121901ef5
khlukekim <khlukekim@gmail.com>
2014-10-17 16:19:02 +0900

    undo implemted

--- homework_new/src/app.js
+++ homework_new/src/app.js
@@ -1,6 +1,8 @@
 
 var HelloWorldLayer = cc.Layer.extend({
     sprite:null,
+    shapes:[],
+    drawNode:null,
     ctor:function () {
       
         this._super();
@@ -17,9 +19,24 @@ var HelloWorldLayer = cc.Layer.extend({
         helloLabel.y = 100;
         this.addChild(helloLabel);
 
+        var closeItem = new cc.MenuItemImage(
+        		res.CloseNormal_png,
+        		res.CloseSelected_png,
+        		this.undo, this);
+        closeItem.attr({
+        	x: 100,
+        	y: 20,
+        	anchorX: 0.5,
+        	anchorY: 0.5
+        });
+
+        var menu = new cc.Menu(closeItem);
+        menu.x = 0;
+        menu.y = 0;
+        this.addChild(menu, 1);
         
-        var drawNode = new cc.DrawNode();
-        this.addChild(drawNode);
+        this.drawNode = new cc.DrawNode();
+        this.addChild(this.drawNode);
         
         var listener = cc.EventListener.create({
         	event:cc.EventListener.TOUCH_ONE_BY_ONE,
@@ -28,15 +45,29 @@ var HelloWorldLayer = cc.Layer.extend({
         		return true;
         	},
         	onTouchMoved:function(touch,event){
-        		var p1 = touch.getPreviousLocation();
+        		return true;
+        	},
+        	onTouchEnded:function(touch,event){
+        		var p1 = touch.getStartLocation();
         		var p2 = touch.getLocation();
-        		drawNode.drawSegment(p1,p2,2,cc.color(150,20,20));
+        		event.getCurrentTarget().shapes.push(p1,p2);
+        		event.getCurrentTarget().drawNode.drawSegment(p1,p2,2,cc.color(100,20,20));
         		return true;
         	}
         });
         cc.eventManager.addListener(listener, this);
       
+        
+        
         return true;
+    },
+    undo: function(){
+    	this.shapes.pop();
+    	this.shapes.pop();
+    	this.drawNode.clear();
+    	for(var i = 0; i<this.shapes.length;i+=2){
+    		this.drawNode.drawSegment(this.shapes[i],this.shapes[i+1],2,cc.color(100,20,20));
+    	}
     }
 });
 
