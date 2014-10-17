9d20fafa1528517cee2eba481de108d0b43fe738
khlukekim <khlukekim@gmail.com>
2014-10-17 16:01:47 +0900

    line drawing implemented

--- homework_new/src/app.js
+++ homework_new/src/app.js
@@ -16,8 +16,27 @@ var HelloWorldLayer = cc.Layer.extend({
         helloLabel.x = this.width/2;
         helloLabel.y = 100;
         this.addChild(helloLabel);
+
+        
+        var drawNode = new cc.DrawNode();
+        this.addChild(drawNode);
+        
+        var listener = cc.EventListener.create({
+        	event:cc.EventListener.TOUCH_ONE_BY_ONE,
+        	swallowTouches:true,
+        	onTouchBegan:function(touch,event){
+        		return true;
+        	},
+        	onTouchMoved:function(touch,event){
+        		var p1 = touch.getPreviousLocation();
+        		var p2 = touch.getLocation();
+        		drawNode.drawSegment(p1,p2,2,cc.color(150,20,20));
+        		return true;
+        	}
+        });
+        cc.eventManager.addListener(listener, this);
+      
         return true;
-//      
     }
 });
 
